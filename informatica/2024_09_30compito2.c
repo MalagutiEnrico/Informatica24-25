e2ff618051462dd662d98c5b2945307afaf7ff11
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2024-10-01 07:54:36 +0000

    correzione

--- informatica/2024_09_30compito2.c
+++ informatica/2024_09_30compito2.c
@@ -1,40 +1,36 @@
 // Dati 3 numeri stampare il più piccolo, il più grande e la loro media aritmetica.
 #include <stdio.h>
 int main(){
-    int n1, n2, n3, media;
-    printf("Inserisci il primo numero: ");
-    scanf("%d", &n1);
-    printf("Inserisci il secondo numero: ");
-    scanf("%d", &n2);
-    printf("Inserisci il terzo numero: ");
-    scanf("%d", &n3);
+    float n1, n2, n3, media;
+    printf("Inserisci i 3 numeri: ");
+    scanf("%f%f%f", &n1, &n2, &n3);
     if(n1 > n2 && n1 > n3){
         if(n2 > n3){
-            printf("Il numero maggiore è %d mentre il numero minore è %d\n", n1, n3);
+            printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n1, n3);
         }
         else{
-            printf("Il numero maggiore è %d mentre il numero minore è %d\n", n1, n2);
+            printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n1, n2);
         }
     }
     else{
         if(n2 > n1 && n2 > n3){
             if(n1>n3){
-                printf("Il numero maggiore è d mentre il numero minore è %d\n", n2, n3);
+                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n2, n3);
             }
             else{
-                printf("Il numero maggiore è %d mentre il numero minore è %d\n", n2, n1);
+                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n2, n1);
             }
         }
         else{
             if(n1 > n2){
-                printf("Il numero maggiore è %d mentre il numero minore è %d\n", n3, n2);
+                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n3, n2);
             }
             else{
-                printf("Il numero maggiore è %d mentre il numero minore è %d\n", n3, n1);
+                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n3, n1);
             }
         }
     }
     media = (n1 + n2 + n3)/3;
-    printf("La media dei tre numeri è: %d\n", media);
+    printf("La media dei tre numeri è: %.2f\n", media);
     return 0;
 }
