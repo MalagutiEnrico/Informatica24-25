e5a161989583cfa61bd64396afe4cc9b352ba867
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2024-10-07 11:11:11 +0000

    correine esercizio

--- informatica/2024_09_30compito2.c
+++ informatica/2024_09_30compito2.c
@@ -1,35 +1,29 @@
 // Dati 3 numeri stampare il più piccolo, il più grande e la loro media aritmetica.
 #include <stdio.h>
 int main(){
-    float n1, n2, n3, media;
+    float n1, n2, n3, media, max, min;
     printf("Inserisci i 3 numeri: ");
     scanf("%f%f%f", &n1, &n2, &n3);
-    if(n1 > n2 && n1 > n3){
-        if(n2 > n3){
-            printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n1, n3);
-        }
-        else{
-            printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n1, n2);
-        }
+    max = n1;
+    min = n1;
+    if(max<n2){
+        max = n2;
+        if(max<n3)
+            max = n3;
     }
-    else{
-        if(n2 > n1 && n2 > n3){
-            if(n1>n3){
-                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n2, n3);
-            }
-            else{
-                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n2, n1);
-            }
-        }
-        else{
-            if(n1 > n2){
-                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n3, n2);
-            }
-            else{
-                printf("Il numero maggiore è %.2f mentre il numero minore è %.2f\n", n3, n1);
-            }
-        }
+    else   
+        if(max<n3)
+            max = n3;
+    printf("il massimo è: %.2f\n", max);
+    if(min>n2){
+        min = n2;
+        if(min > n3)
+            min = n3;
     }
+    else
+        if(min>n3)
+            min = n3;
+    printf("Il minimo è: %.2f\n", min);
     media = (n1 + n2 + n3)/3;
     printf("La media dei tre numeri è: %.2f\n", media);
     return 0;
