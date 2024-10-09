38bce86d086b14d40e3fdc08fd808b7cd0f5b9f7
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2024-10-09 14:15:04 +0000

    esercizio da finire

---
+++ informatica/2024_10_09compito1.c
@@ -0,0 +1,9 @@
+/*Scrivi un programma che permetta all’utente di inserire 5 numeri interi e verifichi se questi numeri appartengono alla sequenza 
+dei numeri naturali, cioè sono tutti differenti e hanno tra di loro distanza unitaria nella linea dei numeri. 
+(ad esempio la sequenza 3-4-5-6-7 è una sequenza adeguata, 3-4-5-7-8 non è una sequenza adeguata)*/
+#include <stdio.h>
+int main(){
+    int n1, n2, n3, n4, n5;
+    printf("Inserisci i 5 numeri: ");
+    scanf("%d%d%d%d%d", &n1, &n2, &n3, &n4, &n5);
+}
