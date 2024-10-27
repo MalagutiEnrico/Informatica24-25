f6003af928e56e6602293d9ccf4dfb67d4e9fcdf
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2024-10-27 15:11:12 +0000

    esercizio indovina numero

---
+++ informatica/2024_10_26compito2.c
@@ -0,0 +1,9 @@
+/*Dato un numero controllare se è divisibile o meno per 3 applicando la regola della 
+somma delle cifre che compongono il numero. ES: dato di input: 123 eseguo la somma 
+delle sue cifre -> 1+2+3=6 ->  risultato 6 -> ok 6 è multiplo di 3.NNB: 
+qualora il numero fosse a più cifre vi fermate alla prima somma delle cifre e poi 
+controllate se il risultato è divisibile per 3.*/
+#include <stdio.h>
+int main(){
+    
+}
