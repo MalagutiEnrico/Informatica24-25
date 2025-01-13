1787006cf4ebace0112a77b9f249467346a6d4e5
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-01-13 10:54:34 +0000

    esercizi sui cicli

--- informatica/2025_01_12compito2.c
+++ informatica/2025_01_12compito2.c
@@ -1,10 +1,16 @@
 /*Scrivi un programma che visualizzi la somma dei quadrati dei primi 12 numeri naturali successivi a 8*/
 #include <stdio.h>
 int main(){
-    int somma_quadrati=0;
-    for(int i=9; i<=20; i++){
+    int somma_quadrati=0; 
+    int inizio=0;
+    int quantita=0;
+    printf("Inserisci il numero d'inizio: ");
+    scanf("%d", &inizio);
+    printf("Inserisci quanti elementi vuoi considerare: ");
+    scanf("%d", &quantita);
+    for(int i=inizio; i<=inizio+quantita; i++){
         somma_quadrati += i*i;
     }
-    printf("La somma dei quadrati dei primi 12 numeri dopo 8 è %d\n", somma_quadrati);
+    printf("La somma dei quadrati dei primi %d numeri dopo %d è %d\n", quantita, inizio, somma_quadrati);
     return 0;
 }
