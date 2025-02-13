4b30824c02298a236163bbfefc5572a605a85a8c
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-02-13 09:34:48 +0000

    tris

--- gioco_tris.c
+++ gioco_tris.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-char matrice[7][7] = {
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ',},
-    {' ',' ', '|', ' ', '|', ' ', ' '},
-    {' ', '-', '+', '-', '+', '-', ' ',},
-    {' ',' ', '|', ' ', '|', ' ', ' '},
-    {' ', '-', '+', '-', '+', '-', ' ',},
-    {' ',' ', '|', ' ', '|', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ',},
-};
+#include <stdlib.h>
 
-char matrice_aggiornata[7][7] = {
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ',},
-    {' ',' ', '|', ' ', '|', ' ', ' '},
+char matrice[7][7] = {
+    {' ', '1', ' ', '3', ' ', '5', ' ',},
+    {'1', ' ', '|', ' ', '|', ' ', ' '},
     {' ', '-', '+', '-', '+', '-', ' ',},
-    {' ',' ', '|', ' ', '|', ' ', ' '},
+    {'3', ' ', '|', ' ', '|', ' ', ' '},
     {' ', '-', '+', '-', '+', '-', ' ',},
-    {' ',' ', '|', ' ', '|', ' ', ' '},
+    {'5', ' ', '|', ' ', '|', ' ', ' '},
     {' ', ' ', ' ', ' ', ' ', ' ', ' ',},
 };
 
@@ -46,24 +38,50 @@ char scegli_simbolo(char *_simbolo){
 }
 
 void inserisci_simbolo(char *_simbolo, int *_x, int *_y){
-    printf("Inserisci la riga in cui posizionare il simbolo: ");
-    scanf("%d", &*_x);
-    printf("Inserisci la colonna in cui posizionare il simbolo: ");
-    scanf("%d", &*_y);
-    matrice_aggiornata[*_x][*_y] = *_simbolo;
-}
-
-void stampa_matrice_aggiornata(){
-    for(int x=0; x<7; x++){
-        for(int y=0; y<7; y++){
-            printf("%c   ", matrice_aggiornata[x][y]);
+    do{
+        printf("Inserisci la riga in cui posizionare il simbolo: ");
+        scanf("%d", &*_x);
+        printf("Inserisci la colonna in cui posizionare il simbolo: ");
+        scanf("%d", &*_y);
+        if(controllo_inserimento(&_x, &_y)==1){
+            printf("L'input inserito non valido. Ritenta.");
         }
-        printf("\n\n");
-    }
+    }while(controllo_inserimento(&_x, &_y)!=0);
+    matrice[*_x][*_y] = *_simbolo;
 }
 
 int controllo_inserimento(int *_x, int *_y){
+    if(*_x==2 || *_y==2 || *_x==4 || *_y==4 || matrice[*_x][*_y]!=' '){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
 
+int controllo_vittoria(){
+    if((matrice[1][1]==matrice[3][3] && matrice[3][3]==matrice[5][5]) || (matrice[1][5]==matrice[3][3] && matrice[3][3]==matrice[5][1])){   //controllo diagonale
+        return 1;
+    }
+    else if((matrice[1][1]==matrice[1][3] && matrice[1][3]==matrice[1][5]) || (matrice[3][1]==matrice[3][3] && matrice[3][3]==matrice[3][5]) || (matrice[5][1]==matrice[5][3] && matrice[5][3]==matrice[5][5])){  //controllo righe
+        return 1;
+    }
+    else if((matrice[1][1]==matrice[3][1] && matrice[3][1]==matrice[5][1]) || (matrice[1][3]==matrice[3][3] && matrice[3][3]==matrice[5][3]) || (matrice[1][5]==matrice[3][5] && matrice[3][5]==matrice[5][5])){    //controllo colonne
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void pulisci_matrice(){
+    for(int x=0; x<7; x++){
+        for(int y=0; y<7; y++){
+            if(matrice[x][y]!='|' || matrice[x][y]!='+' || matrice[x][y]!='-' || matrice[x][y]!=' '){
+                matrice[x][y] = ' ';
+            }
+        }
+    }
 }
 
 int main(){
@@ -74,13 +92,15 @@ int main(){
         scanf("%d", &scelta);
         if(scelta==1){
             scegli_simbolo(&simbolo);
-            stampa_griglia();
-            inserisci_simbolo(&simbolo, &x, &y);
-            stampa_matrice_aggiornata();
+            do{
+                system("CLEAR");
+                stampa_griglia();
+                inserisci_simbolo(&simbolo, &x, &y);
+            }while(controllo_vittoria()!=0);
         }
         else{
             printf("Il programma è terminato.\n");
         }
+        printf("Ha vinto il giocatore %c\n", simbolo);
     }while(scelta!=0);
-}
-    
+}  
