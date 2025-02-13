8d5d085bd345c2e91c0d200de6a5d1e09ba563a0
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-02-13 10:50:17 +0000

    Tris

--- gioco_tris.c
+++ gioco_tris.c
@@ -31,23 +31,10 @@ char scegli_simbolo(char *_simbolo){
     do{
         printf("Inserisci il simbolo del giocatore che inzia: ");
         scanf(" %c", &*_simbolo);
-        if(*_simbolo != 'X' && *_simbolo != 'O' && *_simbolo != 'x' && *_simbolo != 'o'){
+        if(*_simbolo != 'x' && *_simbolo != 'o'){
             printf("Simbolo non valido. Ritenta\n");
         }
-    }while(*_simbolo != 'X' && *_simbolo != 'O' && *_simbolo != 'x' && *_simbolo != 'o');
-}
-
-void inserisci_simbolo(char *_simbolo, int *_x, int *_y){
-    do{
-        printf("Inserisci la riga in cui posizionare il simbolo: ");
-        scanf("%d", &*_x);
-        printf("Inserisci la colonna in cui posizionare il simbolo: ");
-        scanf("%d", &*_y);
-        if(controllo_inserimento(&_x, &_y)==1){
-            printf("L'input inserito non valido. Ritenta.");
-        }
-    }while(controllo_inserimento(&_x, &_y)!=0);
-    matrice[*_x][*_y] = *_simbolo;
+    }while(*_simbolo != 'x' && *_simbolo != 'o');
 }
 
 int controllo_inserimento(int *_x, int *_y){
@@ -59,14 +46,37 @@ int controllo_inserimento(int *_x, int *_y){
     }
 }
 
+void scambia_simbolo(char *_simbolo1, char *_simbolo2){
+    char temp;
+    temp = *_simbolo1;
+    *_simbolo1 = *_simbolo2;
+    *_simbolo2 = temp;
+}
+
+void inserisci_simbolo(char *_simbolo, int *_x, int *_y){
+    do{
+        printf("Inserisci la riga in cui posizionare il simbolo: ");
+        scanf("%d", &*_x);
+        printf("Inserisci la colonna in cui posizionare il simbolo: ");
+        scanf("%d", &*_y);
+        if(controllo_inserimento(&*_x, &*_y)==1){
+            printf("L'input inserito non valido. Ritenta.\n");
+        }
+    }while(controllo_inserimento(&*_x, &*_y)!=0);
+    matrice[*_x][*_y] = *_simbolo;
+}
+
 int controllo_vittoria(){
-    if((matrice[1][1]==matrice[3][3] && matrice[3][3]==matrice[5][5]) || (matrice[1][5]==matrice[3][3] && matrice[3][3]==matrice[5][1])){   //controllo diagonale
+    //controllo diagonale
+    if((matrice[1][1]==matrice[3][3] && matrice[3][3]==matrice[5][5] && matrice[1][1]!=' ') || (matrice[1][5]==matrice[3][3] && matrice[3][3]==matrice[5][1] && matrice[1][5]!=' ')){
         return 1;
     }
-    else if((matrice[1][1]==matrice[1][3] && matrice[1][3]==matrice[1][5]) || (matrice[3][1]==matrice[3][3] && matrice[3][3]==matrice[3][5]) || (matrice[5][1]==matrice[5][3] && matrice[5][3]==matrice[5][5])){  //controllo righe
+    //controllo righe
+    else if((matrice[1][1]==matrice[1][3] && matrice[1][3]==matrice[1][5] && matrice[1][1]!=' ') || (matrice[3][1]==matrice[3][3] && matrice[3][3]==matrice[3][5] && matrice[3][1]!=' ') || (matrice[5][1]==matrice[5][3] && matrice[5][3]==matrice[5][5] && matrice[5][1]!=' ')){  
         return 1;
     }
-    else if((matrice[1][1]==matrice[3][1] && matrice[3][1]==matrice[5][1]) || (matrice[1][3]==matrice[3][3] && matrice[3][3]==matrice[5][3]) || (matrice[1][5]==matrice[3][5] && matrice[3][5]==matrice[5][5])){    //controllo colonne
+    //controllo colonne
+    else if((matrice[1][1]==matrice[3][1] && matrice[3][1]==matrice[5][1] && matrice[1][1]!=' ') || (matrice[1][3]==matrice[3][3] && matrice[3][3]==matrice[5][3] && matrice[1][3]!=' ') || (matrice[1][5]==matrice[3][5] && matrice[3][5]==matrice[5][5] && matrice[1][5]!=' ')){
         return 1;
     }
     else{
@@ -77,30 +87,49 @@ int controllo_vittoria(){
 void pulisci_matrice(){
     for(int x=0; x<7; x++){
         for(int y=0; y<7; y++){
-            if(matrice[x][y]!='|' || matrice[x][y]!='+' || matrice[x][y]!='-' || matrice[x][y]!=' '){
+            if(matrice[x][y] == 'x' || matrice[x][y] == 'o'){
                 matrice[x][y] = ' ';
             }
+            printf("%c   ", matrice[x][y]);
         }
+        printf("\n\n");
     }
 }
 
 int main(){
-    int scelta, x=0, y=0;
-    char simbolo;
+    int scelta, x=0, y=0, controllo, cnt=0;
+    char simbolo, simbolo_invertito;
     do{
         stampa_menu();
         scanf("%d", &scelta);
         if(scelta==1){
+            pulisci_matrice();
             scegli_simbolo(&simbolo);
             do{
-                system("CLEAR");
+                system("clear");
                 stampa_griglia();
                 inserisci_simbolo(&simbolo, &x, &y);
-            }while(controllo_vittoria()!=0);
+                cnt++;
+                if(simbolo == 'x'){
+                    simbolo_invertito='o';
+                }
+                else{
+                    simbolo_invertito='x';
+                }
+                scambia_simbolo(&simbolo, &simbolo_invertito);
+                controllo = controllo_vittoria();
+            }while(controllo!=1 && cnt!=9);
         }
         else{
             printf("Il programma è terminato.\n");
         }
-        printf("Ha vinto il giocatore %c\n", simbolo);
+        scambia_simbolo(&simbolo, &simbolo_invertito);
+        if(cnt==9 && controllo!=1){
+            printf("I giocatori hanno pareggiato\n");
+        }
+        else{
+            printf("Ha vinto il giocatore %c\n", simbolo);
+        }
     }while(scelta!=0);
-}  
+    return 0;
+}
