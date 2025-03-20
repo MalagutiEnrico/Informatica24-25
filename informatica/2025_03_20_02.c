7d39f3f5eabf0979292df00c0e56f87b25d8231e
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-03-20 10:44:03 +0000

    esercizio

--- informatica/2025_03_20_02.c
+++ informatica/2025_03_20_02.c
@@ -3,3 +3,12 @@
 #include "Arraylib.h"
 #include "Arraylib.c"
 
+#define DIM 10
+
+int main(){
+    int vettore[DIM];
+    riempiVettoreCasuale(vettore, DIM, 0, 100);
+    stampaVettore(vettore, DIM, ' ');
+    printf("\nLa media degli elementi del vettore è %.2f\n", mediaVettore(vettore, DIM));
+    return 0;
+}
