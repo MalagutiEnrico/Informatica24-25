e1158818058aa18a05a238338ab5be9d6152cb36
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-03-20 09:53:28 +0000

    esercizio array

---
+++ informatica/2025_03_20_01.c
@@ -0,0 +1,13 @@
+/*Inizializzazione - Riempimento con valori pari-Stampa
+Creazione di una libreria personale*/
+#include <stdio.h>
+#include "Arraylib.h"
+#include "Arraylib.c"
+
+#define DIM 10
+
+int main(){
+    int vettore[10];
+    
+    return 0;
+}
